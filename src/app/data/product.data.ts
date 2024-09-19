c22f8bbfb93bec8a2b7b07dda36f02ecbea5a6f1
prueba2@gmail.com <prueba@gmail.com>
2024-09-19 12:05:26 -0500

    Data de los productos y Service

---
+++ src/app/data/product.data.ts
@@ -0,0 +1,40 @@
+import { Product } from "../models/product";
+
+export const products: Product[] = [
+    {
+      id: 1,
+      name: 'Teclado mecánico Asus RGB',
+      description: 'Algún teclado mecánico con luces RGB cherry red',
+      price: 1000  
+    },
+    {
+        id: 2,
+        name: 'Samsung Smart TV LED 75',
+        description: 'TV con resolución para películas en HD y 4K',
+        price: 2000  
+      },
+      {
+        id: 3,
+        name: 'Sony Camara Video',
+        description: 'Alguna camara de video para streaming',
+        price: 31000  
+      },
+      {
+        id: 4,
+        name: 'Corsair memorias ram 16 GB DDR5',
+        description: 'Memoria RAM optimizada para videojuegos',
+        price: 1800  
+      },
+      {
+        id: 5,
+        name: 'Nvidia ASUS RTX4900',
+        description: 'Tarjeta de video Nvida optimizada para tareas multicore video juegos en 4k',
+        price: 1000  
+      },
+      {
+        id: 6,
+        name: 'CPU Intel core i7',
+        description: 'CPU optimizada para tareas multicore',
+        price: 1000  
+      }
+]
