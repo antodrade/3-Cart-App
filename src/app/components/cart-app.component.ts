2734a8608d89b1f540c96b4de9fa6e75eca45cdf
prueba2@gmail.com <prueba@gmail.com>
2024-09-21 13:47:37 -0500

    Actualizar la cantidad del producto

--- src/app/components/cart-app.component.ts
+++ src/app/components/cart-app.component.ts
@@ -2,21 +2,44 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { Product } from '../models/product';
 import { CatalogComponent } from './catalog/catalog.component';
+import { CartComponent } from './cart/cart.component';
+import { CartItem } from '../models/cartItem';
 
 @Component({
   selector: 'cart-app',
   standalone: true,
-  imports: [CatalogComponent],
+  imports: [CatalogComponent, CartComponent],
   templateUrl: './cart-app.component.html',
 })
 export class CartAppComponent implements OnInit{
 
   products: Product[] = [];
+  items: CartItem[] = [];
+
 constructor(private service: ProductService){
 
 } 
   ngOnInit(): void {
    this.products = this.service.findAll();
+  }
+
+  onAddCart(product: Product){
+  const hasItem = this.items.find(item => {
+    return item.product.id == product.id;
+  })
+  if (hasItem){
+   this.items = this.items.map(item => {
+    if (item.product.id === product.id){
+      return {
+        ...item, quantity: item.quantity +1
+      }
+    }
+    return item;
+   })
+  } else{
+
+    this.items= [... this.items, {product: {...product}, quantity: 1}]
+  }
   }
 
 }
--- src/app/components/catalog/catalog.component.ts
+++ src/app/components/catalog/catalog.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from '../../models/product';
 import { ProductCardComponent } from '../product-card/product-card.component';
 
@@ -12,4 +12,9 @@ export class CatalogComponent {
 
 @Input() products!: Product[];
 
+@Output() productEventEmitter: EventEmitter<Product> = new EventEmitter();
+
+onAddCart(product: Product){
+this.productEventEmitter.emit(product);
+}
 }
